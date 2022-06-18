ebdad8a1742e3bcd7b71f4524953276182ae5163
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-06-18 17:38:02 +0300

    Change function
    1. Add code to work with script without argument.

--- space_manual_bot.py
+++ space_manual_bot.py
@@ -13,11 +13,20 @@ def main():
     parser = create_parser()
     id_arg = parser.parse_args()
     folder = id_arg.argument
-    img_list = get_img(folder)
-    img = random.choice(img_list)
-    way = "images/{}/{}".format(folder, img)
-    bot.send_document(chat_id=-1001381382770, document=open(way, 'rb'))
-
+    default_argument = 0
+    if folder != default_argument:
+        way = "images/{}".format(folder)
+        bot.send_document(chat_id=-1001381382770, document=open(way, 'rb'))
+    else:
+        full_pack = []
+        full_pack += get_img("epic")
+        full_pack += get_img("apod")
+        full_pack += get_img("spacex")
+        folder_img = random.choice(os.listdir("images"))
+        img = random.choice([x for x in os.listdir("images/{}".format(folder_img)) 
+                            if os.path.isfile(os.path.join("images/{}".format(folder_img), x))])
+        way_two = f"images/{folder_img}/{img}"
+        bot.send_document(chat_id=-1001381382770, document=open(way_two, 'rb'))
 
 if __name__ == "__main__":
     main()
