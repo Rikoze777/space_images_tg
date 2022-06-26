cda1a43027190fae243e05a70610cad0e41839c4
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-06-26 15:23:04 +0300

    Delete argparse function

--- help_func.py
+++ help_func.py
@@ -1,14 +1,4 @@
-import argparse
 import os
-
-
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('argument', nargs='?', default=0,
-                        help='Required argument')
-    parser.add_argument('number', nargs='?', const=14400, 
-                        help='Required number')
-    return parser
 
 
 def to_list(dict):
