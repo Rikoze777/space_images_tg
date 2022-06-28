553d2cddcd047e340e01a509fd09f8c8dfe824ba
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-06-28 22:59:02 +0300

    Chat_id to .env

--- space_auto_bot.py
+++ space_auto_bot.py
@@ -8,17 +8,18 @@ import time
 import random
 
 
-def publish(folder, img_list, num, bot):
+def publish(folder, img_list, num, bot, chat_id):
     while True:
         random.shuffle(img_list)
         for image in img_list:
             path = "images/{}/{}".format(folder, image)
-            bot.send_document(chat_id=-1001381382770, document=open(path, 'rb'))
+            bot.send_document(chat_id=chat_id, document=open(path, 'rb'))
             time.sleep(3600*num)
 
 
 def main():
     load_dotenv()
+    chat_id = os.environ.get("CHAT_ID")
     tg_token = os.environ.get("TG_TOKEN")
     bot = telegram.Bot(token=tg_token)
     parser = argparse.ArgumentParser()
@@ -31,7 +32,7 @@ def main():
     folder = args.folder
     num = args.number
     img_list = get_img(folder)
-    publish(folder, img_list, num, bot)
+    publish(folder, img_list, num, bot, chat_id)
 
 
 if __name__ == "__main__":
