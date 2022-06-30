1835fc848c6957de155d98138d800149553630ee
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-06-30 22:20:20 +0300

    Change code
    1. Rename variables
    2. Image path to env
    3. New unpack without index

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,39 +1,41 @@
 import requests
 import os
 import argparse
+from dotenv import load_dotenv
 
 
-def fetch_spacex_launch(id_token):
+def fetch_spacex_launch(launch_id, img_path):
     spacex_url = "https://api.spacexdata.com/v3/launches"
-    path = "images/spacex/"
-    os.makedirs(path, exist_ok=True)
-    param = {
-        "flight_id": id_token
+    os.makedirs(img_path, exist_ok=True)
+    params = {
+        "flight_id": launch_id
     }
-    response = requests.get(spacex_url, params=param)
+    response = requests.get(spacex_url, params=params)
     response.raise_for_status()
     spacex_items = response.json()
     for item in spacex_items:
         urls = item['links']['flickr_images']
         for count, url in enumerate(urls):
-            file_ext = os.path.splitext(url)
-            filename = "spacex_{}{}".format(count, file_ext[1])
-            response_img = requests.get(url)
-            response_img.raise_for_status()
-            path_file = os.path.join(path, filename)
-            with open(path_file, 'wb') as img_file:
-                img_file.write(response_img.content)
+            split_url = os.path.splitext(url)
+            head_url, ext = split_url
+            filename = "spacex_{}{}".format(count, ext)
+            img_response = requests.get(url)
+            img_response.raise_for_status()
+            file_path = os.path.join(img_path, filename)
+            with open(file_path, 'wb') as img_file:
+                img_file.write(img_response.content)
 
 
 def main():
+    load_dotenv()
+    img_path = os.environ.get("SPACEX_PATH")
     parser = argparse.ArgumentParser()
     parser.add_argument('argument',
                         help='Required argument')
     args = parser.parse_args()
     launch_id = args.argument
-    fetch_spacex_launch(launch_id)
+    fetch_spacex_launch(launch_id, img_path)
 
 
 if __name__ == '__main__':
     main()
-    
